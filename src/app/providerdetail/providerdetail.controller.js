8b072df27a6985eab536815bb4f1b74e57aed0cd
Stefan Urech <stefan@surech.ch>
2016-01-08 18:37:32 +0100

    stu: Upload funktioniert!

--- src/app/providerdetail/providerdetail.controller.js
+++ src/app/providerdetail/providerdetail.controller.js
@@ -5,15 +5,20 @@
     .module('ponrWeb')
     .controller('ProviderDetailController', ProviderDetailController);
 
-  ProviderDetailController.$inject = ['$log', 'toastr', '$filter', '$scope'];
+  ProviderDetailController.$inject = ['$log', 'toastr', '$http'];
 
   /** @ngInject */
-  function ProviderDetailController($log, toastr, $filter, $scope) {
+  function ProviderDetailController($log, toastr, $http) {
     var vm = this;
 
+    // Definiert den Status der QR-Code anzeige
     vm.state = '';
 
-    vm.formdata = new FormData();
+    // Wahr, wenn es beim Upload des QR-Codes ein Fehler gab.
+    vm.qrcodeError = false;
+
+    // Hochgeladener Inhalt des QR-Codes
+    vm.qrcodeContent = '';
 
     vm.ViewState = {
       VIEW: 1,
@@ -34,9 +39,48 @@
       vm.state = vm.ViewState.VIEW;
     };
 
+    vm.cancelUpload = function () {
+      vm.qrcodeError = false;
+      vm.qrcodeContent = '';
+      vm.state = vm.ViewState.VIEW;
+    };
+
     vm.addFile = function ($files) {
       vm.files = $files;
-      console.log($files);
+
+      var formdata = new FormData();
+      formdata.append('qrcode', $files[0]);
+
+      // Anfrage zusammenbauen
+      var request = {
+        method: 'POST',
+        url: 'http://api.poinzofnoreturn.ch/qrcode/scanner',
+        data: formdata,
+        headers: {
+          'Content-Type': undefined
+        }
+      };
+
+      // Anfrage auslösen
+      $http(request).success(function(result){
+        vm.qrcodeContent = result.content;
+        vm.qrcodeError = false;
+
+        vm.showUpload();
+      }).error(function(error){
+        vm.qrcodeError = true;
+      })
     };
+
+    vm.saveUpload = function(){
+      // Speichern
+      // TODO
+
+      // Provider anpassen
+      // TODO
+
+      // View aktualisieren
+      vm.cancelUpload();
+    }
   }
 })();
